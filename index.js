17959267d96dc0ff0f229145c67f37a2ad63a9f1
milanla <milan.lanlada@gmail.com>
2018-11-05 13:38:28 -0500

    Done.

--- index.js
+++ index.js
@@ -2,24 +2,19 @@ function BoardMember(name, homeState, training) {
   this.name = name
   this.homeState = homeState
   this.training = training
-
-  BoardMember.prototype.veto = function veto() {
+  this.veto = function veto() {
     return 'No, I must disagree'
   }
-
-  BoardMember.prototype.approve = function approve() {
+  this.approve = function approve() {
     return 'You can do that!'
   }
-
-  BoardMember.prototype.doCharity = function doCharity() {
+  this.doCharity = function doCharity() {
     return 'I like to help people.'
   }
-
-  BoardMember.prototype.releasePressStatement = function releasePressStatement() {
+  this.releasePressStatement = function releasePressStatement() {
     return 'You will see great things from Scuber.'
   }
-
-  BoardMember.prototype.sayHi = function sayHi() {
+  this.sayHi = function sayHi() {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
